Causal Mask Fixed

train_v4_seed_overfit_sds.py
@@ -135,7 +135,7 @@ class SEEDTrainingWrapper(LightningModule):
 
         # For SDS
         t_range = [0.2, 0.6]
-        # t_range = [0.02, 0.98]
+        t_range = [0.02, 0.98]
         self.num_train_timesteps = 1000
         self.min_step = int(self.num_train_timesteps * t_range[0])
         self.max_step = int(self.num_train_timesteps * t_range[1])
@@ -147,11 +147,11 @@ class SEEDTrainingWrapper(LightningModule):
         self.sample_image_ind = 0
         self.logged_original_image = set()
 
+
+    def setup(self, stage):
         for p in self.image_tokenizer.parameters():
             p.requires_grad = False
 
-
-    def setup(self, stage):
         # Setup training parameter
         self.image_tokenizer.model.train()
         for param in self.image_tokenizer.model.parameters():
@@ -159,22 +159,23 @@ class SEEDTrainingWrapper(LightningModule):
 
         for param in self.image_tokenizer.model.Qformer.parameters():
             param.requires_grad = False
-        # # Freeze ViT Encoder
-        # for param in self.image_tokenizer.model.visual_encoder.parameters():
-        #     param.requires_grad = False
-        #
-        # # Diffusion frozen
-        # for param in self.image_tokenizer.diffusion_model.image_encoder.parameters():
-        #     param.requires_grad = False
-        # for param in self.image_tokenizer.diffusion_model.image_normalizer.parameters():
-        #     param.requires_grad = False
-        # for param in self.image_tokenizer.diffusion_model.text_encoder.parameters():
-        #     param.requires_grad = False
-        # # In this case, unet is frozen
-        # for param in self.image_tokenizer.diffusion_model.unet.parameters():
-        #     param.requires_grad = False
-        # for param in self.image_tokenizer.diffusion_model.vae.parameters():
-        #     param.requires_grad = True
+
+        # Freeze ViT Encoder
+        for param in self.image_tokenizer.model.visual_encoder.parameters():
+            param.requires_grad = False
+
+        # Diffusion frozen
+        for param in self.image_tokenizer.diffusion_model.image_encoder.parameters():
+            param.requires_grad = False
+        for param in self.image_tokenizer.diffusion_model.image_normalizer.parameters():
+            param.requires_grad = False
+        for param in self.image_tokenizer.diffusion_model.text_encoder.parameters():
+            param.requires_grad = False
+        # In this case, unet is frozen
+        for param in self.image_tokenizer.diffusion_model.unet.parameters():
+            param.requires_grad = True
+        for param in self.image_tokenizer.diffusion_model.vae.parameters():
+            param.requires_grad = False
 
         # For fp16
         if self.cfg.optimizer.fp16:
@@ -735,6 +736,7 @@ class SEEDTrainingWrapper(LightningModule):
 
         clip_size_image = self.transform_224(batch.img)
         # For cosine similarity logging
+        # gt_txt_clip_embeddings = self.get_clip_text_embedding(batch.gt_txt)
         gt_img_clip_embeddings = self.get_clip_img_embedding(clip_size_image.float())
         with torch.no_grad():
             causal_embeddings = self.get_causal_embeddings(clip_size_image)
@@ -819,12 +821,13 @@ class SEEDTrainingWrapper(LightningModule):
 
         # gt_img_clip_embeddings = self.get_clip_img_embedding(batch.img)
         # loss = F.mse_loss(quant, gt_img_clip_embeddings)
-
-        # params = OrderedDict(self.image_tokenizer.model.Qformer.named_parameters())
-        # grads = torch.autograd.grad(stage_diffusion_loss, params.values(), allow_unused=True)
+        
+        loss = loss_sds + loss_recon + loss_embed
+        # params = OrderedDict(self.image_tokenizer.model.quantize.named_parameters())
+        # grads = torch.autograd.grad(loss, params.values(), allow_unused=True)
         # print(grads)
 
-        return loss_sds + loss_recon + loss_embed
+        return loss
 
     def validation_step(self, batch, batch_idx: int):
         image = self.transform_224(batch.img)
@@ -981,6 +984,9 @@ if __name__ == "__main__":
     datamodule.setup()
     train_dataloader = datamodule.train_dataloader()
     val_dataloader = datamodule.val_dataloader()
+
+    print(f"### Number of train: {len(train_dataloader)}")
+    print(f"### Number of valid: {len(val_dataloader)}")
 
     tb_logger = pl_loggers.TensorBoardLogger(save_dir=cfg.result_file_path)
     lr_logger = pl.callbacks.LearningRateMonitor(logging_interval="step")

train_v7_seed_stage1_training.py
@@ -66,6 +66,26 @@ NUM_IMG_TOKNES = 32
 NUM_IMG_CODES = 8192
 IMAGE_ID_SHIFT = 32000
 
+class GatherLayer(torch.autograd.Function):
+    """
+    Gather tensors from all workers with support for backward propagation:
+    This implementation does not cut the gradients as torch.distributed.all_gather does.
+    """
+
+    @staticmethod
+    def forward(ctx, x):
+        output = [
+            torch.zeros_like(x) for _ in range(torch.distributed.get_world_size())
+        ]
+        torch.distributed.all_gather(output, x)
+        return tuple(output)
+
+    @staticmethod
+    def backward(ctx, *grads):
+        all_gradients = torch.stack(grads)
+        torch.distributed.all_reduce(all_gradients)
+        return all_gradients[torch.distributed.get_rank()]
+
 class SEEDTrainingWrapper(LightningModule):
     """Training wrapper for SEED
 
@@ -82,7 +102,7 @@ class SEEDTrainingWrapper(LightningModule):
                             from_pretrained=True,
                             diffusion_model_path=cfg.checkpoint_path.diffusion_model_path,
                             load_diffusion=False,
-                            is_train_stage_1=False,
+                            is_train_stage_1=True,
                             )
 
         self.B = None
@@ -344,6 +364,22 @@ class SEEDTrainingWrapper(LightningModule):
 
         return reverse_output_proj, loss_embed, embed_ind
     
+    def all_gather_with_grad(self, tensors):
+        """
+        Performs all_gather operation on the provided tensors.
+        Graph remains connected for backward grad computation.
+        """
+        # Queue the gathered tensors
+        world_size = torch.distributed.get_world_size()
+        # There is no need for reduction in the single-proc case
+        if world_size == 1:
+            return tensors
+
+        # tensor_all = GatherLayer.apply(tensors)
+        tensor_all = GatherLayer.apply(tensors)
+
+        return torch.cat(tensor_all, dim=0)
+    
     @torch.no_grad()
     def concat_all_gather(self, tensor):
         """
@@ -394,7 +430,7 @@ class SEEDTrainingWrapper(LightningModule):
             encoder_attention_mask=image_atts,
             use_cache=True,
             return_dict=True,
-            attention_mask=causal_mask,  # Apply causal mask here
+            # attention_mask=causal_mask,  # Apply causal mask here
         )
 
         # Use last hidden state
@@ -571,6 +607,7 @@ class SEEDTrainingWrapper(LightningModule):
                 F.cross_entropy(sim_i2t, targets, label_smoothing=0.1)
                 + F.cross_entropy(sim_t2i, targets, label_smoothing=0.1)
             ) / 2
+            print(f"Loss I2T: {loss_itc}")
 
             self.log(
                     "val/loss_itc",
@@ -648,10 +685,12 @@ class SEEDTrainingWrapper(LightningModule):
 
         ###============== Image-text Contrastive ===================###
         # Compute for each query token
-        image_feats_all = self.concat_all_gather(
+        # image_feats_all = self.concat_all_gather(
+        image_feats_all = self.all_gather_with_grad(
             image_feats
         )  # [batch_size*num_gpu, num_query_tokens, embed_dim]
-        text_feat_all = self.concat_all_gather(text_feat)  # [batch_size*num_gpu, embed_dim]
+        # text_feat_all = self.concat_all_gather(text_feat)  # [batch_size*num_gpu, embed_dim]
+        text_feat_all = self.all_gather_with_grad(text_feat)  # [batch_size*num_gpu, embed_dim]
 
         # image_feats.unsqueeze(1) : [batch_size, 1, num_query_tokens, embed_dim]
         # text_feat_all.unsqueeze(-1) : [batch_size*num_gpu, embed_dim, 1] => broadcast to [batch_size, batch_size*num_gpu, embed_dim, 1]
@@ -999,9 +1038,10 @@ if __name__ == "__main__":
     )
 
     wrapper = SEEDTrainingWrapper(cfg).to(device)
-    # wrapper = SEEDTrainingWrapper.load_from_checkpoint(
-    #     "/home/zheedong/Projects/SEED/logs/seed_stage_1_training_debug/lightning_logs/version_61_load_from_60/checkpoints/epoch=2-step=1656.ckpt",
-    #     cfg=cfg).to(device)
+    wrapper = SEEDTrainingWrapper.load_from_checkpoint(
+        "/home/zheedong/Projects/SEED/logs/seed_stage_1_training_debug/lightning_logs/version_50_stage_1_final_token_init_weight_new_version/checkpoints/epoch=31-step=25024.ckpt",
+        # "/home/zheedong/Projects/SEED/logs/seed_stage_1_training_debug/lightning_logs/version_61_load_from_60/checkpoints/epoch=2-step=1656.ckpt",
+        cfg=cfg).to(device)
     wrapper.setup("fit")
 
     trainer.fit(